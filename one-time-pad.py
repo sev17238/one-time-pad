45f3b8c66527133932b63e8f3ef842def8f7d615
Diego Sevilla <sevilla.diego8@gmail.com>
2021-03-01 23:02:01 -0600

    feat: convertion to binary has errors

--- one-time-pad.py
+++ one-time-pad.py
@@ -7,6 +7,8 @@
 # Python3 program to....
 ######################################################
 
+# tools ___________________________
+# https://onlinestringtools.com/convert-string-to-ascii
 
 # imports _________________________
 import random
@@ -34,6 +36,55 @@ def string_to_binary(message):
     binaryMessage = ''.join(format(ord(i), '08b') for i in message) 
     #binaryMessage = ''.join(format(i, '08b') for i in bytearray(message, encoding ='utf-8')) 
     return binaryMessage
+
+def string_to_ascii(test_list):
+    #test_list = ['gfg', 'is', 'best'] 
+    
+    # printing original list  
+    print("The original list : " + str(test_list)) 
+
+    # Convert String list to ascii values using loop + ord() 
+    res = [] 
+    for ele in test_list: 
+        res.extend(ord(num) for num in ele) 
+    return res
+
+def ssss(string):
+    result = list(bytes(b'test'))
+    return result
+
+def binary_to_decimal(binary): 
+    # Using int function to convert to string    
+    string = int(binary, 2) 
+    return string 
+      
+def BynaryToDecimal(b = '10001111100101110010111010111110011'):
+    # Driver's code 
+    # initializing binary data 
+    bin_data = b
+    print("The binary value is:", bin_data) 
+    str_data =' '
+    
+    # slicing the input and converting it  
+    # in decimal and then converting it in string 
+    for i in range(0, len(bin_data), 7): 
+        
+        # slicing the bin_data from index range [0, 6] 
+        # and storing it in temp_data 
+        temp_data = bin_data[i:i + 7] 
+        
+        # passing temp_data in BinarytoDecimal() function 
+        # to get decimal value of corresponding temp_data 
+        decimal_data = binary_to_decimal(temp_data) 
+        
+        # Deccoding the decimal value returned by  
+        # BinarytoDecimal() function, using chr()  
+        # function which return the string corresponding  
+        # character for given ASCII value, and store it  
+        # in str_data 
+        str_data = str_data + chr(decimal_data)  
+
+    return str_data
 
 
 def xor_operation(string1,string2):
@@ -71,9 +122,15 @@ def main():
         message = f.read()
         print(message.split(',')) 
 
-        print(string_to_binary('Hi'))
         print(randon_binary_string())
         print(xor_operation('0101','1010'))
+
+        print(string_to_ascii(['hello',' ','world']))
+
+        print(string_to_binary(' Geeks'))
+        print(BynaryToDecimal(string_to_binary(' Geeks')))
+
+
 
     except ValueError:
         print("Check the input parameters. Try again...")
