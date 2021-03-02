db4419014c1742fd869aac8e9ca80e1121a64b4f
Diego Sevilla <sevilla.diego8@gmail.com>
2021-03-01 21:02:18 -0600

    feat: :zap: Template python file

---
+++ test.py
@@ -0,0 +1,50 @@
+######################################################
+# Luis Delgado 17
+# Diego Sevilla 17238
+######################################################
+# test.py
+######################################################
+# Python3 program to....
+######################################################
+
+# imports _________________________
+
+
+
+# Definitions _________________________
+oneTimePad = 'cool'
+
+
+# Functions area______________________________________________
+def convertion_from_afn_to_afd(param1,param2):
+    '''
+    Test function
+
+    Params:
+     - param1: sdjfaosdjkf
+     - param2: askdjflaklsdjfa
+    '''
+
+    return 0
+
+
+
+# Main______________________________________________
+def main():
+    try:
+        number = 1
+        print(number)
+
+    except ValueError:
+        print("Check the input parameters. Try again...")
+
+    # print(x.get_string(fields=["Estado"])[0])
+    # print(x)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+#python afn_to_afd.py afn-tran-table.csv
